c77cae85b58d2dc31177a24bf76022a08f0a432a
Fadjri <moefadjri@gmail.com>
2025-04-22 10:57:52 +0700

    Add files via upload

---
+++ login_smk/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login - SMK Bunda Kandung</title>
+</head>
+<body>
+  <h2 align="center">Form Login</h2>
+  <form action="login.php" method="post" align="center">
+    <input type="text" name="username" placeholder="username" required><br></br>
+    <input type="password" name="password" placeholder="password" required><br></br>
+    <input type="submit" value="Login">
+  </form>
+</body>
+</html>
